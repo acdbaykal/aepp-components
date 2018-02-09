9061887e215b1b96850c29cccd6be2c383d0975a
ali <abaykal@tutanota.com>
2018-02-09 11:15:25 +0100

    should fail

--- src/components/aeValidatedTextInput/index.js
+++ src/components/aeValidatedTextInput/index.js
@@ -1,5 +1,6 @@
 import aeValidatedTextInput from './aeValidatedTextInput.vue'
 
 export default function install (Vue) {
+  console.log('ldfjhsdlfls')
   Vue.component('ae-validated-text-input', aeValidatedTextInput)
 }
